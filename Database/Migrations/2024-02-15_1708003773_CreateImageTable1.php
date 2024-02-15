0a53efc7f428f51bc3453d0c14dba86f79d4a76a
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-02-15 22:57:42 +0900

    🗃️  add: images schema

---
+++ Database/Migrations/2024-02-15_1708003773_CreateImageTable1.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Migrations;
+
+use Database\SchemaMigration;
+
+class CreateImageTable1 implements SchemaMigration
+{
+    public function up(): array
+    {
+        // マイグレーションロジックをここに追加してください
+        return [
+            "CREATE TABLE IF NOT EXISTS images (
+                    id INT PRIMARY KEY AUTO_INCREMENT,
+                file_path VARCHAR(255) NOT NULL UNIQUE,
+                shared_path VARCHAR(255) NOT NULL UNIQUE,
+                delete_path VARCHAR(255) NOT NULL UNIQUE,
+                id_address VARCHAR(255) NOT NULL,
+                view_count INT DEFAULT 0,
+                file_size INT DEFAULT 0,
+                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                last_accessed_at DATETIME DEFAULT CURRENT_TIMESTAMP
+            )"
+        ];
+    }
+
+    public function down(): array
+    {
+        // ロールバックロジックを追加してください
+        return [
+            "DROP TABLE IF EXISTS images"
+        ];
+    }
+}
