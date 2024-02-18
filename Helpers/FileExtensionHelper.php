253d381fb4f7b210c2969e7d980b622ad3df3d28
S-aoba <writer_fairy_road@yahoo.co.jp>
2024-02-18 17:21:15 +0900

    🚚 mv: url関連の関数を移動

--- Helpers/FileExtensionHelper.php
+++ Helpers/FileExtensionHelper.php
@@ -13,20 +13,5 @@ class FileExtensionHelper
     return $hashedFileName;
   }
 
-  public static function generateSharedURL(string $hashedFileName): string
-  {
-    $domain = $_SERVER['HTTP_HOST'];
-    $parts = explode(".", $hashedFileName);
-    $mediaType = end($parts);
-    $url = "https://{$domain}/shared/{$mediaType}/{$hashedFileName}";
-    return $url;
-  }
-
-  public static function generateDeleteURL(string $fileType){
-    // ランダムなハッシュ値を生成する
-    $hashedFileName = hash('sha256', uniqid());
-    $domain = $_SERVER['HTTP_HOST'];
-    $url = "https://{$domain}/delete/{$fileType}/{$hashedFileName}";
-    return $url;
-  }
+ 
 }
--- Routing/routes.php
+++ Routing/routes.php
@@ -3,6 +3,7 @@
 use Helpers\DatabaseHelper;
 use Helpers\ValidationHelper;
 use Helpers\FileExtensionHelper;
+use Helpers\URLHelper;
 
 use Response\HTTPRenderer;
 use Response\Render\HTMLRenderer;
@@ -22,21 +23,21 @@ return [
     // 1日の最大容量5MB
     $ipAddress = $_SERVER['REMOTE_ADDR'];
     error_log($ipAddress);
-    if(!DatabaseHelper::checkDailyUploadLimitExceeded($ipAddress)) return new JSONRenderer(["status" => "1日あたりのアップロード容量を超えました。1日最大容量は、5MBです。"]);
+    if (!DatabaseHelper::checkDailyUploadLimitExceeded($ipAddress)) return new JSONRenderer(["status" => "1日あたりのアップロード容量を超えました。1日最大容量は、5MBです。"]);
     // アップロードされた画像のファイルの種類を確認する(対応可能拡張子: jpg, jpeg, png, gif)
-    if(!ValidationHelper::checkFileExtension($file_type)) return new JSONRenderer(["status" => "アップロードされたファイルの拡張子が対応していません。"]);
+    if (!ValidationHelper::checkFileExtension($file_type)) return new JSONRenderer(["status" => "アップロードされたファイルの拡張子が対応していません。"]);
     // アップロードされた画像のファイルサイズを確認する　一回のアップロードの最大サイズ3MBに設定
-    if(!DatabaseHelper::checkUploadFileSize($file_size)) return new JSONRenderer(["status" => "アップロードされたファイルのサイズが3MBを超えています。"]);
+    if (!DatabaseHelper::checkUploadFileSize($file_size)) return new JSONRenderer(["status" => "アップロードされたファイルのサイズが3MBを超えています。"]);
     // 画像のファイル名をハッシュ化する hash値.拡張子
     $hashed_file_name = FileExtensionHelper::hashedFileName($file_name);
 
     // shared_urlを生成する {https://{domain}/{media-type}/{unique-string}}
-    $shared_url = FileExtensionHelper::generateSharedURL($hashed_file_name);
+    $shared_url = URLHelper::generateSharedURL($hashed_file_name);
 
     // delete_urlを生成する
-    $delete_url = FileExtensionHelper::generateDeleteURL($file_type);
+    $delete_url = URLHelper::generateDeleteURL($file_type);
     // DBのimagesテーブルに画像を保存する
-    return new JSONRenderer(["status" => "Image uploaded", 'shared_url' => 'https://www.google.com', "ip_address"=> $ipAddress]);
+    return new JSONRenderer(["status" => "Image uploaded", 'shared_url' => 'https://www.google.com', "ip_address" => $ipAddress]);
   }
   // Imageの作成
   // 作成したImageの表示
