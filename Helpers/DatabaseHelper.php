dad25952805e051acc325744f7d3a120d93d5b15
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-22 12:48:25 +0900

    Add savaUploadImage method

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -6,5 +6,27 @@ use Database\MySQLWrapper;
 
 class DatabaseHelper
 {
-   
+   public static function saveUploadImage(array $data, string $imagePath, string $deletePath, string $uniqueToken): array {
+    $db = new MySQLWrapper();
+
+    $stmt = $db->prepare("INSERT INTO images (name, image_path, delete_path, size, mime_type, unique_token) VALUES (?, ?, ?, ?, ?, ?)");
+
+    $stmt->bind_param('sssiss', $data['name'], $imagePath, $deletePath, $data['size'], $data['type'], $uniqueToken);
+    $stmt->execute();
+
+    if ($stmt->execute()) {
+      $insertId = $db->insert_id;
+
+      return [
+          'success' => true,
+          'insert_id' => $insertId,
+          'message' => 'Image saved successfully.'
+      ];
+    } else {
+      return [
+          'success' => false,
+          'message' => 'Failed to save image: ' . $stmt->error
+      ];
+    }
+  }
 }
