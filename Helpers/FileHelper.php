78d5b10a4d1e881a6adf3c33860ee6c309c6d70c
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-22 12:34:45 +0900

    Add FileHelper Class

---
+++ Helpers/FileHelper.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Helpers;
+
+class FileHelper {
+  public static function formatFileData(array $file): array {
+    $formatList = ['name', 'type', 'size'];
+    $formattedFile = [];
+
+    foreach($file as $key => $val) {
+      if(in_array($key, $formatList)) {
+        if($key === 'name') {
+          // nameを50文字に切り取る。ユーザーが入力する形に変更したら削除する
+          $newVal = mb_substr($file['name'], 0, 49, 'UTF-8');
+          $formattedFile[$key] = $newVal;
+          continue;
+        }
+        $formattedFile[$key] = $val;
+      }
+    }
+    
+    return $formattedFile;
+  }
+
+  public static function generateImagePath(string $filename): string {
+    $dateStr = date('Ymd');
+    $uniqueStr = uniqid();
+
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+    return $dateStr . '_' . $uniqueStr . '.' . $ext;
+  }
+
+  public static function checkImageStorageDirectory(): void {
+    $privatDir = 'private';
+    if(is_dir($privatDir) === false) {
+      mkdir($privatDir);
+    }
+
+    $imgDir = 'private/uploads';
+    if(is_dir($imgDir) === false) {
+      mkdir($imgDir);
+    }
+
+    return;
+  }
+
+  public static function generateDeletePath(string $filename): string {
+    $salt = 'delete_at';
+    return hash_hmac('sha256', $filename, $salt);
+  }
+
+  public static function generateUniqueTokenPath(string $filename): string {
+    return hash('sha256', $filename);
+  }
+}
