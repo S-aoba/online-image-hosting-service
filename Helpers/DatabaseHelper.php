b41732bb41347affd8605fb709b25ecfd765e430
ts_dev <writer_fairy_road@yahoo.co.jp>
2025-01-24 13:58:23 +0900

    Add delete upload from db and remove execute code in saveUploadImage method

--- Helpers/DatabaseHelper.php
+++ Helpers/DatabaseHelper.php
@@ -12,7 +12,6 @@ class DatabaseHelper
     $stmt = $db->prepare("INSERT INTO images (name, image_path, delete_path, size, mime_type, unique_token) VALUES (?, ?, ?, ?, ?, ?)");
 
     $stmt->bind_param('sssiss', $data['name'], $imagePath, $deletePath, $data['size'], $data['type'], $uniqueToken);
-    $stmt->execute();
 
     if ($stmt->execute()) {
       $insertId = $db->insert_id;
@@ -28,5 +27,30 @@ class DatabaseHelper
           'message' => 'Failed to save image: ' . $stmt->error
       ];
     }
+  }
+
+  // public static function getUploadImage(string $imagePath) : array {
+  //   $db = new MySQLWrapper();
+
+  //   $stmt = $db->prepare("SELECT * FROM images WHERE image_path = ?");
+  //   $stmt->bind_param('s', $imagePath);
+  //   $stmt->execute();
+
+  //   $result = $stmt->get_result();
+  //   $image = $result->fetch_assoc();
+
+  //   return $image;
+  // }
+
+  public static function deleteUploadImage(string $deletePath): bool {
+    $db = new MySQLWrapper();
+
+    $stmt = $db->prepare("DELETE FROM images WHERE delete_path = ?");
+    $stmt->bind_param('s', $deletePath);
+    $stmt->execute();
+
+    $affectRows = $stmt->affected_rows;
+    
+    return $affectRows > 0;
   }
 }
